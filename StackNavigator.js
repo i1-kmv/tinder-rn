d456496a16ee9f4fa70d2a1da929a153cfbc2130
i1-kmv <i1-kmv@mail.ru>
2022-02-22 22:26:25 +0300

    creaate HpmePage header

--- StackNavigator.js
+++ StackNavigator.js
@@ -13,7 +13,10 @@ export const StackNavigator = () => {
     const { user} = useAuth()
 
     return (
-       <Stack.Navigator>
+       <Stack.Navigator
+         screenOptions={{
+           // headerShown: false
+       }}>
            {user ? (
            <>
                <Stack.Screen name="Home" component={HomeScreen}/>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,28 +1,45 @@
 import React from "react";
-import {View, Text, Button} from "react-native";
+import {
+    View,
+    Text,
+    Button,
+    SafeAreaView,
+    TouchableOpacity,
+    Image
+} from "react-native";
 import {useNavigation} from "@react-navigation/native";
 import useAuth from "../hooks/useAuth";
+import tw from "tailwind-rn";
+import {AntDesign, Entypo, Ionicons} from "@expo/vector-icons";
 
 
 export const HomeScreen = () => {
 
     const navigation = useNavigation()
-    const {logout} = useAuth()
+    const {logout, user} = useAuth()
 
     return (
-        <View>
-            <Text>
-                HomeScreen
-            </Text>
-            <Button
-                title="go to chat"
-                onPress={() => navigation.navigate("Chat")}
-            />
-            <Button
-                title="logout"
-                onPress={logout}
-            />
-        </View>
+        <SafeAreaView style={tw('pt-7')}>
+            <View  style={tw("flex-row items-center justify-between px-5")}>
+               <TouchableOpacity onPress={logout}>
+                    <Image
+                        style={tw('h-10 w-10 rounded-full')}
+                        source={{uri: user.photoURL}}
+                    />
+               </TouchableOpacity>
+
+                <TouchableOpacity>
+                    <Image
+                        style={tw('h-14 w-14 rounded-full')}
+                        source={require("../logo.png")}
+                    />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
+                    <Ionicons name='chatbubbles-sharp' size={30}/>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
     )
 }
 
