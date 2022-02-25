a998605b3f4e8992f55c381f564576325a413016
i1-kmv <i1-kmv@mail.ru>
2022-02-25 10:11:33 +0300

    finish modalScreen

--- screens/ModalScreen.js
+++ screens/ModalScreen.js
@@ -1,15 +1,47 @@
-import React, {useState} from "react";
+import {doc, setDoc, serverTimestamp} from "firebase/firestore"
+import React, {useLayoutEffect, useState} from "react";
 import {View, Text, Image, TextInput, TouchableOpacity} from "react-native";
 import tw from "tailwind-rn";
 import useAuth from "../hooks/useAuth";
+import {useNavigation} from "@react-navigation/native";
+import {db} from "../firebase";
 
 
 export const ModalScreen = () => {
 
     const {user} = useAuth()
+    const navigation = useNavigation();
     const [image, setImage] = useState(null)
     const [job, setJob] = useState(null)
     const [age, setAge] = useState(null)
+
+    const incompleteForm = !image || !job || !age
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: true,
+            headerTitle: "Update your profile",
+            headerStyle: {
+                backgroundColor: "#FF5864",
+            },
+            headerTitleStyle: {color: "white"}
+        })
+    },[])
+
+    const updateUserProfile = () => {
+        setDoc(doc(db, 'users', user.uid), {
+            id: user.uid,
+            displayName: user.displayName,
+            photoURL: image,
+            job,
+            age,
+            timestamp: serverTimestamp()
+        }).then(() => {
+            navigation.navigate('Home')
+        }).catch(error => {
+            alert(error.message)
+        })
+    }
 
 
     return (
@@ -52,10 +84,18 @@ export const ModalScreen = () => {
                 onChangeText={text => setAge(text)}
                 style={tw("text-center text-xl pb-2")}
                 placeholder="Enter a Age"
+                keyboardType="numeric"
+                maxLength={2}
             />
 
-            <TouchableOpacity style={tw('w-64 p-3 rounded-xl absolute bottom-10 bg-red-400')}>
-                <Text style={tw("text-center text-white text-xl")}>Update Profile</Text>
+            <TouchableOpacity
+                disabled={incompleteForm}
+                style={[
+                    tw('w-64 p-3 rounded-xl mt-24'),
+                    incompleteForm ? tw('bg-gray-400') : tw('bg-red-400')]}
+                onPress={updateUserProfile}
+            >
+                <Text style={[tw("text-center text-white text-xl"), ]}>Update Profile</Text>
             </TouchableOpacity>
         </View>
     )
